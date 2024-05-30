af9cf3f3387fca27d9887f35bbb53c3595ddb6ea
Arqam Javaid Khan <arqam60@gmail.com>
2024-05-30 12:26:21 +0500

    temperature adjusted

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_conversational_chain():
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.8)
+                             temperature=0.5)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
